89bb8edce08f5023ed54a6f0503b75760fc8564a
Manickam <maniks1312@gmail.com>
2021-02-10 18:47:50 +0530

    FirstCommit

---
+++ src/components/Pages.js
@@ -0,0 +1,22 @@
+import React , { Component} from 'react';
+
+class Pages extends Component {
+
+    render(){
+
+        return (
+            <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        );
+    }
+}
+
+export default Pages ;
---
+++ src/components/Properties.js
@@ -0,0 +1,13 @@
+import React , { Component} from 'react';
+
+class Properties extends Component {
+
+    render(){
+
+        return (
+            <h1>This is form Properties</h1>
+        );
+    }
+}
+
+export default Properties ;
